b7fe3a6b4da6bb1fb0bb5ce30979566db811c323
Joyus Jose Mathew <joyus.sics@gmail.com>
2020-07-07 16:07:16 +0100

    UAT Second

--- Demo_Git_1/ViewController.swift
+++ Demo_Git_1/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello")
         print("Hi Joyus")
+        print("UAT")
         // Do any additional setup after loading the view.
     }
 
